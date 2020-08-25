10478fe285329ccf09d6609d5f0961f59ef32295
bhaberl <bhaberl.lab@gmail.com>
2020-08-25 22:52:02 +0200

    Add preset of dgn agent and neural-network

---
+++ deep_q_network/model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        pass
